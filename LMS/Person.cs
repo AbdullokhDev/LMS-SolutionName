b6eb9f77b78a2e3f6eeda82e8de5e2c91c0a577d
AbdullokhDev <abdulloh.watch@internet.ru>
2021-10-20 22:10:37 +0500

    Add project files.

---
+++ LMS/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LMS
+{
+    class Person
+    {
+        // Creating fields(proporties) called Name, age, pet for objects
+        public string Name;
+        public int Age;
+        public bool HasPet;
+
+        // Method called Intoduce void keyword is used because of it should not return anything so that we do not use it in future
+        public void Introduce()
+        {
+            // build in function
+            Console.WriteLine("Hello, my name is " + Name + " and my age is " + Age);
+        }
+    }
+}
---
+++ LMS/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LMS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // class name is Person we are referring
+            // variable name is person
+            Person person = new Person();
+            // now to time fill the fields which is proporties in the Person class that we created
+
+            person.Name = "Abdullokh";
+            person.Age = 22;
+            person.HasPet = false;
+
+            person.Introduce();
+        }
+    }
+}
